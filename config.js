55456b0e636b9a83e9b26de15cdd276e8c18c26c
angga69-oss <rasamangga69@gmail.com>
2026-07-17 23:26:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@
 
 const CONFIG = {
   // Hubungkan ke API Generator Gambar Anda (misalnya: Midjourney, Stable Diffusion, DALL-E, atau Gemini Image API)
-  API_IMAGE: "AQ.Ab8RN6IbxoLKmWjzvMLzj9ijuDQJF7iEmSp3_lACc6zOAS9NFg",
+  API_IMAGE: "LkRtY5OAifwblkcAIqY9qZGpb246ETHLWSGnJLRY",
 
   // Hubungkan ke API Generator Video Anda (misalnya: Runway, Luma, Kling, Pika, Sora, dll)
-  API_TEXT_VIDEO: " AQ.Ab8RN6IbxoLKmWjzvMLzj9ijuDQJF7iEmSp3_lACc6zOAS9NFg",
+  API_TEXT_VIDEO: " LkRtY5OAifwblkcAIqY9qZGpb246ETHLWSGnJLRY",
 
   // Hubungkan ke API Image to Video Anda
-  API_IMAGE_VIDEO: "AQ.Ab8RN6IbxoLKmWjzvMLzj9ijuDQJF7iEmSp3_lACc6zOAS9NFg",
+  API_IMAGE_VIDEO: "LkRtY5OAifwblkcAIqY9qZGpb246ETHLWSGnJLRY",
 
   // Konfigurasi Tambahan jika diperlukan (seperti custom headers atau API token)
   API_HEADERS: {
